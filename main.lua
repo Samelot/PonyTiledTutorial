526de7faa4b36ff6dedfeff1dc15e0b1ef2dbd92
Sam Englander <sizeoverload@gmail.com>
2016-05-26 01:05:22 -0400

    started using tiled, played with map:brick()

--- main.lua
+++ main.lua
@@ -18,4 +18,10 @@ physics.start()
 local map = tiled.new(mapData)
 map.x,map.y = display.contentCenterX - map.designedWidth/2, display.contentCenterY - map.designedHeight/2
 
+local brick = map:findObject("topRight")
+--brick.anchorX = 0.5
+--brick.anchorY = 0.5
+--brick:translate(brick.width/2, -brick.height/2)
+transition.to(brick, {rotation = 360, delta = true, iterations = -1})
+
 --map:extend("dragable")
